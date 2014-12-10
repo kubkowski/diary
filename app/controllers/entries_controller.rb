06be491b65f7aac6ae8e2db4bc4555c3b40da2a9
kubkowski <jakub.opara@gmail.com>
2014-12-10 18:30:33 +0100

    Added new and edit controller actions + views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -8,4 +8,25 @@ def show
   @entry = Entry.find(params["id"])
 end
 
+def new
+
+end
+
+def create
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.create(entry_params)
+  redirect_to(entry_path(entry))
+end
+
+def edit
+  @entry = Entry.find(params["id"])
+end
+
+def update
+  entry_params = params["entry"].permit("title", "contents")
+  entry = Entry.find(params["id"])
+  entry.update(entry_params)
+  redirect_to(entry_path(entry))
+end
+
 end
